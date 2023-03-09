2eff11fe4a2900bba8623b130e5a8b4e84560009
momo-github788 <momoZzzz1996@gmail.com>
2023-03-09 11:31:45 -0300

    Added Employee POJO and unit tests

---
+++ src/main/java/za/ac/cput/domain/Employee.java
@@ -0,0 +1,61 @@
+package za.ac.cput.domain;
+
+import java.util.Objects;
+
+public class Employee {
+    private String id;
+    private String firstName;
+    private String lastName;
+    private String emailAddress;
+
+    public Employee() {
+
+    }
+
+    public Employee(String id, String firstName, String lastName, String emailAddress) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.emailAddress = emailAddress;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String getEmailAddress() {
+        return emailAddress;
+    }
+
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Employee employee = (Employee) o;
+        return Objects.equals(id, employee.id) && Objects.equals(firstName, employee.firstName) && Objects.equals(lastName, employee.lastName) && Objects.equals(emailAddress, employee.emailAddress);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, firstName, lastName, emailAddress);
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "id='" + id + '\'' +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", emailAddress='" + emailAddress + '\'' +
+                '}';
+    }
+}
